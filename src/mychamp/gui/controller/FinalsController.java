b78b1525e2d37b2277c9998b319d1c7e2360ba89
LindvedKrvang <rklindved@gmail.com>
2017-01-02 15:21:44 +0100

    Finals Mock up startet

---
+++ src/mychamp/gui/controller/FinalsController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mychamp.gui.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rasmus
+ */
+public class FinalsController implements Initializable {
+
+    @FXML
+    private Label lblRankings;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handlePlayoffButton(ActionEvent event) throws IOException {
+        Stage primStage = (Stage) lblRankings.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mychamp/gui/view/FinalsView.fxml"));
+        Parent root = loader.load();
+        
+        primStage.setScene(new Scene(root));
+    }
+    
+}
