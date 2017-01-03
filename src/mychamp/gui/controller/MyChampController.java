2901ffb1bc513537ed04cf7d07068abf002d06a5
Sk0vgaard <Sk0vgaard@live.dk>
2017-01-03 11:13:18 +0100

    View, Delete
    School is added for editing and you can now delete all teams.

--- src/mychamp/gui/controller/MyChampController.java
+++ src/mychamp/gui/controller/MyChampController.java
@@ -38,7 +38,8 @@ import mychamp.gui.model.TeamModel;
  *
  * @author gta1
  */
-public class MyChampController implements Initializable {
+public class MyChampController implements Initializable
+{
 
     @FXML
     private Label lblTeamAmount;
@@ -64,13 +65,17 @@ public class MyChampController implements Initializable {
     private final TeamModel teamModel;
 
     private TableView.TableViewSelectionModel<Team> selectedView;
-    
-    public MyChampController() {
-        teamModel = TeamModel.getInstance();        
+    @FXML
+    private TextField txtTeamSchool;
+
+    public MyChampController()
+    {
+        teamModel = TeamModel.getInstance();
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         initializeDesign();
         initializeTables();
         setListeners();
@@ -79,100 +84,131 @@ public class MyChampController implements Initializable {
     /**
      * Sets the initial design
      */
-    private void initializeDesign() {
+    private void initializeDesign()
+    {
         lblTeamAmount.setText("0");
     }
 
     /**
      * Fills the tables with information about the teams
      */
-    private void initializeTables() {
+    private void initializeTables()
+    {
         tableTeams.setItems(teamModel.getTeams());
         clmID.setCellValueFactory(new PropertyValueFactory<>("ID"));
         clmTeam.setCellValueFactory(new PropertyValueFactory<>("TEAM_NAME"));
 
     }
-    
+
     /**
      * Adds a listener to tableTeams.
      */
-    private void setListeners(){
-        tableTeams.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Team>() {
+    private void setListeners()
+    {
+        tableTeams.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Team>()
+        {
             @Override
-            public void changed(ObservableValue<? extends Team> observable, Team oldValue, Team newValue) {
-                if(newValue != null){
+            public void changed(ObservableValue<? extends Team> observable, Team oldValue, Team newValue)
+            {
+                if (newValue != null)
+                {
                     displayTeamInfo();
                 }
             }
         });
     }
-    
+
     /**
      * Display the information of the team.
      */
-    private void displayTeamInfo(){
+    private void displayTeamInfo()
+    {
         Team team = (Team) tableTeams.getSelectionModel().getSelectedItem();
         txtTeamID.setText(team.getID() + "");
         txtTeamName.setText(team.getTEAM_NAME());
         txtTeamField.setText(team.getHOME_FIELD());
+        txtTeamSchool.setText(team.getSCHOOL());
     }
 
     @FXML
-    private void handleEditSelectedTeam(ActionEvent event) {
+    private void handleEditSelectedTeam(ActionEvent event)
+    {
+
     }
 
-    private Alert removeManyItems() {
+    private Alert removeManyItems()
+    {
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Bekræftelsesdialog");
         alert.setHeaderText("Er du sikker på du vil slette holdene?");
         alert.setContentText("Tryk 'OK' for at slette.");
         return alert;
     }
-    
-    private Alert teamRemoveDialog(Team teamToDelete) {
+
+    private Alert teamRemoveDialog(Team teamToDelete)
+    {
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Bekræftelsesdialog");
         alert.setHeaderText("Er du sikker på du vil slette holdet: " + "\n\n" + teamToDelete.getTEAM_NAME());
         alert.setContentText("Tryk 'OK' for at slette.");
         return alert;
     }
-    
+
     @FXML
-    private void handleDeleteSelectedTeam(ActionEvent event) {
+    private void handleDeleteSelectedTeam(ActionEvent event)
+    {
         ObservableList<Team> teamsToDelete = tableTeams.getSelectionModel().getSelectedItems();
         Alert alert;
-        if (teamsToDelete.size() > 1) {
+        if (teamsToDelete.size() > 1)
+        {
             alert = removeManyItems();
-        } else {
+        } else
+        {
             alert = teamRemoveDialog(teamsToDelete.get(0));
         }
-        
+
         Optional<ButtonType> result = alert.showAndWait();
-        
-        if (result.get() == ButtonType.OK) {
-        teamModel.deleteTeam(teamsToDelete);
+
+        if (result.get() == ButtonType.OK)
+        {
+            teamModel.deleteTeam(teamsToDelete);
+            if (tableTeams.getSelectionModel().isEmpty())
+            {
+                txtTeamName.clear();
+                txtTeamField.clear();
+                txtTeamSchool.clear();
+                txtTeamID.clear();
+            } else
+            {
+                displayTeamInfo();
+            }
         }
     }
-    
+
     /**
      * Select more than one team
      */
     @FXML
-    private void handleMultiSelect(KeyEvent event) {
-        if (event.isControlDown() | event.isShiftDown()) {
+    private void handleMultiSelect(KeyEvent event)
+    {
+        if (event.isControlDown() | event.isShiftDown())
+        {
             tableTeams.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        } else {
+        } else
+        {
             tableTeams.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         }
     }
-    
+
     /**
      * Switches to the PlayOffView.
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
-    private void handleStartTournament(ActionEvent event) throws IOException {
+    private void handleStartTournament(ActionEvent event) throws IOException
+    {
         Stage primStage = (Stage) txtTeamName.getScene().getWindow();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/mychamp/gui/view/PlayOffView.fxml"));
         Parent root = loader.load();
@@ -182,32 +218,37 @@ public class MyChampController implements Initializable {
 
     /**
      * Adds a Team.
+     *
      * @param event
      */
     @FXML
-    private void handleAddTeam(ActionEvent event) {
+    private void handleAddTeam(ActionEvent event)
+    {
         //Check to see if all information is present.
-        if(!txtNewTeamName.getText().equals("") 
-                || !txtNewTeamField.getText().equals("") 
-                || !txtNewTeamSchool.getText().equals("")){
+        if (!txtNewTeamName.getText().equals("")
+                || !txtNewTeamField.getText().equals("")
+                || !txtNewTeamSchool.getText().equals(""))
+        {
             //Creates a team.
             teamModel.addTeam(new Team(
-                txtNewTeamName.getText(), 
-                txtNewTeamField.getText(), 
-                txtNewTeamSchool.getText()));
+                    txtNewTeamName.getText(),
+                    txtNewTeamField.getText(),
+                    txtNewTeamSchool.getText()));
             //Clears the fields for information.
             txtNewTeamName.clear();
             txtNewTeamField.clear();
             txtNewTeamSchool.clear();
-        }else{
+        } else
+        {
             warningDialog();
         }
     }
-    
+
     /**
      * Pops up a warning dialog telling the user, there are missing information.
      */
-    private void warningDialog() {
+    private void warningDialog()
+    {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setHeaderText("Advarsel");
         alert.setTitle("Manglende information.");
