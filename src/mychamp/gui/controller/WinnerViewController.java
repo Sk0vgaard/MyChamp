bd58944fec33a89dea47e3d3bbe5eabf1bca49e8
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-01-12 16:05:19 +0100

    Made a winnerview

---
+++ src/mychamp/gui/controller/WinnerViewController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mychamp.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mathias
+ */
+public class WinnerViewController implements Initializable {
+
+    @FXML
+    private Label lblWinner;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
